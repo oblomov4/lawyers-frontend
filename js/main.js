3d652ad9a54f6d7f5e4e60fd655b53edad05a8ae
oblomov4 <vlad.oblomov4@gmail.com>
2025-02-08 00:45:18 +0300

    final

--- js/main.js
+++ js/main.js
@@ -48,3 +48,66 @@ const swiper1 = new Swiper('.swiper1', {
     el: '.swiper-scrollbar',
   },
 });
+
+//Работа с burger
+
+const burger = document.querySelector('.burger');
+const mobileMenu = document.querySelector('.header__mobile-menu');
+
+burger.onclick = (e) => {
+  mobileMenu.classList.add('open');
+  document.body.style.overflowY = 'hidden';
+};
+
+const closeMenu = document.querySelector('.close');
+
+if (closeMenu) {
+  closeMenu.onclick = (e) => {
+    mobileMenu.classList.remove('open');
+    document.body.style.overflowY = '';
+  };
+}
+
+const panelLink = document.querySelectorAll('.nav__panel-a');
+
+panelLink.forEach(
+  (elem) =>
+    (elem.onclick = (e) => {
+      mobileMenu.classList.remove('open');
+      document.body.style.overflowY = '';
+    }),
+);
+
+const orderCallBurger = document.getElementById('order-call-burger');
+
+orderCallBurger.onclick = () => {
+  mobileMenu.classList.remove('open');
+  document.body.style.overflowY = '';
+  document.querySelector('.order__call').classList.add('open');
+};
+
+const search = document.getElementById('search');
+const searchBlock = document.querySelector('.search-mobile-wrapper');
+const closeSearch = document.getElementById('close-search');
+
+search.onclick = (e) => {
+  searchBlock.classList.add('open');
+};
+
+closeSearch.onclick = () => {
+  searchBlock.classList.remove('open');
+};
+
+const orderCallBtn = document.getElementById('order-call');
+const orderCall = document.querySelector('.order__call');
+const orderCallClose = document.querySelector('.order__call-close');
+
+orderCallBtn.onclick = (e) => {
+  orderCall.classList.add('open');
+};
+
+if (orderCallClose) {
+  orderCallClose.onclick = (e) => {
+    orderCall.classList.remove('open');
+  };
+}
